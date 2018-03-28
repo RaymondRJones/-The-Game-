c7113fb88d80620e341e7920f4b4bcd8efb5b06a
Raymond jones <Rayjones2170@gmail.com>
2018-03-27 20:19:26 -0400

    Finishes Cards and Deck creation methods

--- Robin.py
+++ Robin.py
@@ -21,41 +21,20 @@ def shuffle(cards):
 
 def getReward(reward):
     pass
-    #f
 
 #Places card into pile to be discarded and eventually added back into deck
 def discard(card):
     pass
-    #
 
 #Draws a card from a deck of cards
 def drawCard(cards):
     pass
-    #
+
 #removes card from game existence from array
 def burnCard(cards):
     pass
     #
 
-#Creates Hazards with their constructors to be played in the game
-#Returns array filled with Hazards
-def createHazards():
-    pass
-    #
-#Creates fightCards with their constructors to be played in the game
-#Returns array filled with starting fightCards
-def createFightCardsDeck():
-    fightDeck = []
-    for i in range(0, 8):
-        fightDeck.append(Weak())
-    for i in range(0,5):
-        fightDeck.append(Distracted())
-    for i in range(0,3):
-        fightDeck.append(Focused())
-    fightDeck.append(Genius())
-    fightDeck.append(Eating())
-
-    #
 #Creates Deck filled with Aging cards that must be drawn
 def createAgingDeck():
     pass
@@ -115,11 +94,11 @@ class Weak(FightCard):
 #5
 class Distracted(FightCard):
     def __init__(self,name="distracted", fightScore="-1",ability="none"):
-        super().__init__(name, fightScore)
+        super().__init__(name, fightScore, ability)
         self.ability = ability
 class Stupid(FightCard):
     def __init__(self, name = "very stupid", fightScore = -3, ability = "none"):
-        super().__init__(name,fightScore)
+        super().__init__(name,fightScore, ability)
         self.ability = ability
 #3
 class Focused(FightCard):
@@ -210,47 +189,74 @@ class Raft(Hazard):
 class Pirates(Hazard):
     def __init__(self, name, diffScore, Reward,alertLevel):
         super().__init__(name,diffScore,Reward, alertLevel)
+#Creates fightCards with their constructors to be played in the game
+#Returns array filled with starting fightCards
+def createFightCardsDeck():
+    fightDeck = []
+    for i in range(0, 8):
+        fightDeck.append(Weak())
+    for i in range(0,5):
+        fightDeck.append(Distracted())
+    for i in range(0,3):
+        fightDeck.append(Focused())
+    fightDeck.append(Genius())
+    fightDeck.append(Eating())
+    return fightDeck
+#Creates Hazards with their constructors to be played in the game
+#Returns array filled with Hazards
+def createHazardsDeck():
+    # Creates 10 different Raft Cards
+    raft1 = Raft("Raft", "strategy", 0)
+    raft2 = Raft("Raft", "mimicry", 0)
+    raft3 = Raft("Raft", "realization", 0)
+    raft4 = Raft("Raft", "deception", 0)
+    raft5 = Raft("Raft", "equipment", 0)
+    # another unknown effect
+    raft6 = Raft("Raft", "???", 0)
+    raft7 = Raft("Raft", "book", 0)
+    raft8 = Raft("Raft", "food", 0)
+    raft8 = Raft("Raft", "food", 0)
+    raft9 = Raft("Raft", "food", 0)
+    raft10 = Raft("Raft", "equipment", 0)
+    # Creates 8 different Explore Cards
+    explore1 = Explore("Explore", "repeat", 0)
+    explore2 = Explore("Explore", "food", 0)
+    explore3 = Explore("Explore", "deception", 0)
+    explore4 = Explore("Explore", "weapon", 0)
+    explore5 = Explore("Explore", "mimicry", 0)
+    explore6 = Explore("Explore", "food", 0)
+    explore7 = Explore("Explore", "realization", 0)
+    explore8 = Explore("Explore", "weapon", 0)
 
-
+    # Creates 6 different Further Explore Cards
+    furtherExplore1 = FurtherExplore("Further Explore", "realization", 0)
+    furtherExplore2 = FurtherExplore("Further Explore", "experience", 0)
+    furtherExplore3 = FurtherExplore("Further Explore", "food", 0)
+    furtherExplore4 = FurtherExplore("Further Explore", "???Happy", 0)
+    furtherExplore5 = FurtherExplore("Further Explore", "repeat", 0)
+    furtherExplore6 = FurtherExplore("Further Explore", "strategy", 0)
+    # Creates 4 different Animal Cards
+    wild1 = WildAnimal("Wild Animal", "vision", 0)
+    wild2 = WildAnimal("Wild Animal", "experience", 0)
+    wild3 = WildAnimal("Wild Animal", "red", 0)
+    # Not sure what this ability is
+    wild4 = WildAnimal("Wild Animal", "strategy", 0)
+    # Creates 2 Cannibal Cards
+    cannibal1 = Cannibals("Cannibals", "Weapon", 0)
+    cannibal2 = Cannibals("Cannibals", "Weapon", 0)
+    hazardsDeck = [raft1, raft2, raft3, raft4, raft5, raft6, raft7, raft8, raft9, raft10,
+                   explore1, explore2, explore3, explore4, explore5, explore6, explore7, explore8,
+                   furtherExplore1, furtherExplore2, furtherExplore3, furtherExplore4, furtherExplore5, furtherExplore6,
+                   wild1, wild2, wild3, wild4, cannibal1, cannibal2
+                   ]
+    return hazardsDeck
 print("Building Deck...")
 #For Loop to Create Starting Robin Deck
 fightDeck = createFightCardsDeck()
-print(fightDeck)
-#Creates 10 different Raft Cards
-raft1 = Raft("Raft", "strategy", 0)
-raft2 = Raft("Raft", "strategy", 0)
-raft3 = Raft("Raft", "strategy", 0)
-raft4 = Raft("Raft", "strategy", 0)
-raft1 = Raft("Raft", "strategy", 0)
-raft1 = Raft("Raft", "strategy", 0)
-raft1 = Raft("Raft", "strategy", 0)
-raft1 = Raft("Raft", "strategy", 0)
-raft1 = Raft("Raft", "strategy", 0)
-raft1 = Raft("Raft", "strategy", 0)
-raft1 = Raft("Raft", "strategy", 0)
-#Creates 8 different Explore Cards
-explore1 = Explore("Explore", "strategy", 0)
-explore1 = Explore("Explore", "strategy", 0)
-explore1 = Explore("Explore", "strategy", 0)
-explore1 = Explore("Explore", "strategy", 0)
-explore1 = Explore("Explore", "strategy", 0)
-explore1 = Explore("Explore", "strategy", 0)
-explore1 = Explore("Explore", "strategy", 0)
-explore1 = Explore("Explore", "strategy", 0)
-
-#Creates 6 different Further Explore Cards
-FurtherExplore("Further Explore", "vision", 0)
-FurtherExplore("Further Explore", "vision", 0)
-FurtherExplore("Further Explore", "vision", 0)
-FurtherExplore("Further Explore", "vision", 0)
-FurtherExplore("Further Explore", "vision", 0)
-FurtherExplore("Further Explore", "vision", 0)
-#Creates 4 different Animal Cards
-WildAnimal("Wild Animal", "vision", 0)
-#Creates 2 Cannibal Cards
-Cannibals("Cannibals", "Weapon", 0)
-Cannibals("Cannibals", "Weapon", 0)
-print(raft1.reward)
+hazardsDeck = createHazardsDeck()
+print(len(fightDeck))
+print(len(hazardsDeck))
+print(hazardsDeck[1].reward)
 weakCard1 = Weak()
 hungry1 = FightCard("Hungry", 0, "Hungry")
 print(weakCard1.name)
@@ -260,14 +266,20 @@ print("Friday is a game about understanding chance and probability of cards to o
 print("Robinson Crusoe has been stranded on an island for weeks, help guide him against the trecherous hazards")
 robin = Robin()
 while(True):
-    choice = input("What would you like to do?")
     print("1 -> Draw Hazards")
     print("2 -> Use Card Ability")
     print("3 -> Read Card Ability")
     print("4 -> Count Remaining Cards")
+    choice = input("What would you like to do?")
     if(choice == 1):
         #DrawCard(Hazards)
-        #DrawCard(Hazards
+        #DrawCard(Hazards)
+        #Print Hazard Cards name, effect, drawcount, Reward
+        #Ask which card they pick
+        #FightStarted = True
+        #if they want to draw card, enter 1
+        #Ask if they want to concede
+        #Calculate lifepoints
         pass
     elif(choice == 2):
         pass
