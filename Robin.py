988ecd74f2e4d07016d926085f1d6dae09af0ab0
Raymond jones <Rayjones2170@gmail.com>
2018-03-29 11:58:53 -0400

    Adds functions to calculate life and drawCounts

--- Robin.py
+++ Robin.py
@@ -3,7 +3,7 @@ import random
 destroyedCards = []
 hazardDeck = []
 agingDeck = []
-missionCards = []
+missionList = []
 hazardDiscards = []
 fightDeckDiscards = []
 
@@ -59,6 +59,7 @@ class FightCard:
         self.name = name
         self.fightScore = fightScore
         self.tapped = 0
+        self.ability = ability
     #Returns name and effect of ability
     def getAbility(self):
         pass
@@ -66,8 +67,13 @@ class FightCard:
     #Tapps the card
     def tapCard(self):
         self.tapped = 1
+    #Untaps Card
     def unTapCard(self):
         self.tapped = 0
+    def isTapped(self):
+        if(self.tapped == 0):
+            return False
+        return True
     #checks names and details card ability
     def checkAbility(self):
     #Checks cards ability and performs action
@@ -162,17 +168,17 @@ class Hazard:
     #Checks if defeated, then turns into reward
     def convertToReward(self):
         #if reward is "strat" make a strat with 1 fight
-        if(self.name == "raft"):
+        if(self.name == "Raft"):
             freshCard = FightCard(self.reward, 0, self.reward)
-        elif(self.name == "explore"):
+        elif(self.name == "Explore"):
             if(self.reward == "weapon"):
                 freshCard = FightCard("weapon", 2, "none")
             freshCard = FightCard(self.reward, 1, self.reward)
-        elif(self.name == "further explore"):
+        elif(self.name == "Further Explore"):
             freshCard = FightCard(self.reward, 2, self.reward)
-        elif(self.name == "wild animal"):
+        elif(self.name == "Wild Animal"):
             freshCard = FightCard(self.reward, 3, self.reward)
-        elif(self.name == "cannibals"):
+        elif(self.name == "Cannibals"):
             freshCard = FightCard("weapon", 4, "none")
         else:
             freshCard = "error"
@@ -183,7 +189,7 @@ class Hazard:
 class Cannibals(Hazard):
     def __init__(self, name, reward, alertLevel, greenScore=5, yellowScore=9, redScore=14, drawCount=5):
         super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
-        self.name = "wild"
+        self.name = "Cannibals"
         self.reward = "Weapon"
         self.alertLevel = alertLevel
 class WildAnimal(Hazard):
@@ -195,19 +201,19 @@ class WildAnimal(Hazard):
 class FurtherExplore(Hazard):
     def __init__(self, name, reward, alertLevel, greenScore=2, yellowScore=5, redScore=8, drawCount=3):
         super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
-        self.name = "further explore"
+        self.name = "Further Explore"
         self.reward = reward
         self.alertLevel = alertLevel
 class Explore(Hazard):
     def __init__(self, name, reward, alertLevel, greenScore=1, yellowScore=3, redScore=6, drawCount=2):
         super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
-        self.name = "Raft"
+        self.name = "Explore"
         self.reward = reward
         self.alertLevel = alertLevel
 class Raft(Hazard):
     def __init__(self, name, reward, alertLevel, greenScore = 0, yellowScore = 1, redScore = 3, drawCount= 1):
         super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
-        self.name = "raft"
+        self.name = "Raft"
         self.reward = reward
         self.alertLevel = alertLevel
 class Pirates(Hazard):
@@ -217,7 +223,10 @@ def displayFightCard(card):
     print("Name: ", card.name, "Score: ", card.fightScore, "Ability: ", card.ability)
 def displayHazard(card):
     print("Discovery!", card.name, "gives you: ", card.reward, "Score to defeat: ", card.greenScore, "Available draws: ",card.drawCount)
-def startHazard(cards, discards):
+def displayMissionList(missionList):
+    for x in missionList:
+        print(displayFightCard(x))
+def startHazard(cards, discards, fightDeck, fightDeckDiscards, missionList):
     print("Life Points: ", robin.lifePoints)
     displayHazard(cards[0])
     displayHazard(cards[1])
@@ -225,28 +234,58 @@ def startHazard(cards, discards):
     if (choice == 1):
         #Start mission using card of choice
         #Stores unused card into discard pile
-        startMission(drawHazardCard(cards,discards))
+        startMission(drawHazardCard(cards,discards), fightDeck,fightDeckDiscards,missionList)
         #Draws other card from deck and puts into discards
         drawHazardCard(cards,discards)
     else:
         drawHazardCard(cards,discards)
-#COMMENTED OUT UNTIL I FIX
-        #startMission(drawHazardCard(cards,discards))
-    # Function to start the mission
+        startMission(drawHazardCard(cards,discards), fightDeck,fightDeckDiscards,missionList)
+
 #NEEDS FUNCTIONS TO CHECK DRAW COUNT, NEEDS FUNCTIONS TO CALCULATE LIFE
-def startMission(card, fightDeck, fightDeckDiscards):
+#calculates remaining draw count given a hazard cards and mission list cards fight cards
+def calculateDrawCount(hazardCard, missionList):
+    total = hazardCard.drawCount
+    total = total - len(missionList)
+    if(total <= 0):
+        print("No more free draws")
+        return 0
+    return total
+#Difficulty - fight score of all mission cards given a negative
+def calculateLife(hazardCard, missionList):
+    totalDamage = 0
+    if(hazardCard.alertLevel == 0):
+        totalDamage = int(hazardCard.greenScore)
+    elif(hazardCard.alertLevel == 1):
+        totalDamage = int(hazardCard.yellowScore)
+    elif(hazardCard.alertLevel == 2):
+        totalDamage = int(hazardCard.redScore)
+    else:
+        print("There seems to be a problem")
+    for x in missionList:
+        totalDamage = totalDamage - x.fightScore
+    return totalDamage
+def startMission(hazardCard, fightDeck, fightDeckDiscards, missionList):
     while(True):
         choice = int(input("1.)Draw a card  2.) Concede Battle"))
         if(choice == 1):
-            drawFightCard(fightDeck, fightDeckDiscards)
-            displayFightCard()
+            print("---------------DEBUG----------------")
+            displayHazard(hazardCard)
+            print("Available Draws: ",calculateDrawCount(hazardCard, missionList))
+            print("Score needed to win ",calculateLife(hazardCard, missionList))
+            missionList.append(drawFightCard(fightDeck, fightDeckDiscards))
+            displayMissionList(missionList)
+            print("-----------------DEBUG---------------")
         # Draw card from fight deck and put into mission list
         # Display needed fightscore to win, hazard.fightscore - cardfightscore
         elif(choice == 2):
-            pass
+            life = calculateLife(hazardCard, missionList)
             #loseLife()
             # -> Display MissionList Array
-            choice2 = int(input("Enter number of card you want to destroy"))
+            if(life > 0):
+                for i in range(0, life):
+                    choice2 = int(input("Enter number of card you want to destroy"))
+                    missionList.pop(choice2 -1)
+            break;
         # If they concede, Ask to choose cards to burn from mission cards
         # Subtract from their lifepoints the hazard.fightscore - total_fightscore
         # if Difference Score is negative, they win if they concede
@@ -255,7 +294,8 @@ def startMission(card, fightDeck, fightDeckDiscards):
         # Print you won statement
         else:
             print("oops")
-
+    return life
+    #TO DO - FUNCTION RETURNS LIFE, LIFE FUNCTION TAKES AS INPUT TO DECREASE LIFE IF GREATER THAN 0
 #Creates fightCards with their constructors to be played in the game
 #Returns array filled with starting fightCards
 def createFightCardsDeck():
@@ -282,9 +322,9 @@ def createHazardsDeck():
     raft6 = Raft("Raft", "???", 0)
     raft7 = Raft("Raft", "book", 0)
     raft8 = Raft("Raft", "food", 0)
-    raft8 = Raft("Raft", "food", 0)
     raft9 = Raft("Raft", "food", 0)
-    raft10 = Raft("Raft", "equipment", 0)
+    raft10 = Raft("Raft", "food", 0)
+    raft11 = Raft("Raft", "equipment", 0)
     # Creates 8 different Explore Cards
     explore1 = Explore("Explore", "repeat", 0)
     explore2 = Explore("Explore", "food", 0)
@@ -327,15 +367,16 @@ shuffle(hazardsDeck)
 #print("Robinson Crusoe has been stranded on an island for weeks, help guide him against the trecherous hazards")
 robin = Robin()
 while(True):
+    print("-------------------------")
     print("1 -> Draw 2 Hazards")
     print("2 -> Use Card Ability")
     print("3 -> Read Card Ability")
     print("4 -> Count Remaining Cards")
+    print("---------------------------")
     choice = int(input("What would you like to do?"))
     print(choice)
     if(choice == 1):
-       startHazard(hazardsDeck, hazardDiscards)
-
+       startHazard(hazardsDeck, hazardDiscards, fightDeck, fightDeckDiscards, missionList)
     elif(choice == 2):
         pass
     elif(choice == 3):
