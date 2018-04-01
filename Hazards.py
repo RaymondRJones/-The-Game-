049c50fff10f7027dfd2ad02db3ffb2e8763b547
Raymond jones <Rayjones2170@gmail.com>
2018-04-01 13:26:28 -0400

    Tries importing files into Python..Fails

---
+++ Hazards.py
@@ -0,0 +1,117 @@
+import fightcards
+class Hazard:
+
+    #Hazard cards have a name, winScore, and alert level
+    #They also have reward names, which give different abilites
+    #Can be turned into reward with unique fight values
+    def __init__(self, name, reward, alertLevel,  greenScore, yellowScore, redScore, drawCount):
+        self.name = name
+        self.greenScore = greenScore
+        self.yellowScore = yellowScore
+        self.redScore = redScore
+        self.reward = reward
+        self.alertLevel = alertLevel
+        self.drawCount = drawCount
+    #Checks if defeated, then turns into reward
+    def convertToReward(self):
+        #if reward is "strat" make a strat with 1 fight
+        if(self.name == "Raft"):
+            freshCard = FightCard(self.reward, 0, self.reward)
+        elif(self.name == "Explore"):
+            if(self.reward == "weapon"):
+                freshCard = FightCard("weapon", 2, "none")
+            freshCard = FightCard(self.reward, 1, self.reward)
+        elif(self.name == "Further Explore"):
+            freshCard = FightCard(self.reward, 2, self.reward)
+        elif(self.name == "Wild Animal"):
+            freshCard = FightCard(self.reward, 3, self.reward)
+        elif(self.name == "Cannibals"):
+            freshCard = FightCard("weapon", 4, "none")
+        else:
+            freshCard = "error"
+        return freshCard
+    #Increases alert level by 1
+    def increaseAlert(self):
+        self.alertLevel += 1
+    def decreaseAlert(self):
+        self.alert -= 1
+class Cannibals(Hazard):
+    def __init__(self, name, reward, alertLevel, greenScore=5, yellowScore=9, redScore=14, drawCount=5):
+        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
+        self.name = "Cannibals"
+        self.reward = "Weapon"
+        self.alertLevel = alertLevel
+class WildAnimal(Hazard):
+    def __init__(self, name, reward, alertLevel, greenScore=4, yellowScore=7, redScore=11, drawCount=4):
+        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
+        self.name = "Wild Animal"
+        self.reward = reward
+        self.alertLevel = alertLevel
+class FurtherExplore(Hazard):
+    def __init__(self, name, reward, alertLevel, greenScore=2, yellowScore=5, redScore=8, drawCount=3):
+        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
+        self.name = "Further Explore"
+        self.reward = reward
+        self.alertLevel = alertLevel
+class Explore(Hazard):
+    def __init__(self, name, reward, alertLevel, greenScore=1, yellowScore=3, redScore=6, drawCount=2):
+        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
+        self.name = "Explore"
+        self.reward = reward
+        self.alertLevel = alertLevel
+class Raft(Hazard):
+    def __init__(self, name, reward, alertLevel, greenScore = 0, yellowScore = 1, redScore = 3, drawCount= 1):
+        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
+        self.name = "Raft"
+        self.reward = reward
+        self.alertLevel = alertLevel
+class Pirates(Hazard):
+    def __init__(self, name, diffScore, Reward,alertLevel):
+        super().__init__(name,diffScore,Reward, alertLevel)
+#Returns array filled with Hazards
+def createHazardsDeck():
+    # Creates 10 different Raft Cards
+    raft1 = Raft("Raft", "strategy", 0)
+    raft2 = Raft("Raft", "mimicry", 0)
+    raft3 = Raft("Raft", "realization", 0)
+    raft4 = Raft("Raft", "deception", 0)
+    raft5 = Raft("Raft", "equipment", 0)
+    # another unknown effect
+    raft6 = Raft("Raft", "???", 0)
+    raft7 = Raft("Raft", "book", 0)
+    raft8 = Raft("Raft", "food", 0)
+    raft9 = Raft("Raft", "food", 0)
+    raft10 = Raft("Raft", "food", 0)
+    raft11 = Raft("Raft", "equipment", 0)
+    # Creates 8 different Explore Cards
+    explore1 = Explore("Explore", "repeat", 0)
+    explore2 = Explore("Explore", "food", 0)
+    explore3 = Explore("Explore", "deception", 0)
+    explore4 = Explore("Explore", "weapon", 0)
+    explore5 = Explore("Explore", "mimicry", 0)
+    explore6 = Explore("Explore", "food", 0)
+    explore7 = Explore("Explore", "realization", 0)
+    explore8 = Explore("Explore", "weapon", 0)
+
+    # Creates 6 different Further Explore Cards
+    furtherExplore1 = FurtherExplore("Further Explore", "realization", 0)
+    furtherExplore2 = FurtherExplore("Further Explore", "experience", 0)
+    furtherExplore3 = FurtherExplore("Further Explore", "food", 0)
+    furtherExplore4 = FurtherExplore("Further Explore", "vision", 0)
+    furtherExplore5 = FurtherExplore("Further Explore", "repeat", 0)
+    furtherExplore6 = FurtherExplore("Further Explore", "strategy", 0)
+    # Creates 4 different Animal Cards
+    wild1 = WildAnimal("Wild Animal", "vision", 0)
+    wild2 = WildAnimal("Wild Animal", "experience", 0)
+    wild3 = WildAnimal("Wild Animal", "red", 0)
+    # Not sure what this ability is
+    wild4 = WildAnimal("Wild Animal", "strategy", 0)
+    # Creates 2 Cannibal Cards
+    cannibal1 = Cannibals("Cannibals", "Weapon", 0)
+    cannibal2 = Cannibals("Cannibals", "Weapon", 0)
+    hazardsDeck = [raft1, raft2, raft3, raft4, raft5, raft6, raft7, raft8, raft9, raft10,
+                   explore1, explore2, explore3, explore4, explore5, explore6, explore7, explore8,
+                   furtherExplore1, furtherExplore2, furtherExplore3, furtherExplore4, furtherExplore5, furtherExplore6,
+                   wild1, wild2, wild3, wild4, cannibal1, cannibal2
+                   ]
+    return hazardsDeck
--- Robin.py
+++ Robin.py
@@ -1,4 +1,6 @@
 import random
+import fightcards
+import hazards
 
 destroyedCards = []
 agingDeck = []
@@ -12,75 +14,6 @@ block comment
 """
 
 
-class Hazard:
-
-    #Hazard cards have a name, winScore, and alert level
-    #They also have reward names, which give different abilites
-    #Can be turned into reward with unique fight values
-    def __init__(self, name, reward, alertLevel,  greenScore, yellowScore, redScore, drawCount):
-        self.name = name
-        self.greenScore = greenScore
-        self.yellowScore = yellowScore
-        self.redScore = redScore
-        self.reward = reward
-        self.alertLevel = alertLevel
-        self.drawCount = drawCount
-    #Checks if defeated, then turns into reward
-    def convertToReward(self):
-        #if reward is "strat" make a strat with 1 fight
-        if(self.name == "Raft"):
-            freshCard = FightCard(self.reward, 0, self.reward)
-        elif(self.name == "Explore"):
-            if(self.reward == "weapon"):
-                freshCard = FightCard("weapon", 2, "none")
-            freshCard = FightCard(self.reward, 1, self.reward)
-        elif(self.name == "Further Explore"):
-            freshCard = FightCard(self.reward, 2, self.reward)
-        elif(self.name == "Wild Animal"):
-            freshCard = FightCard(self.reward, 3, self.reward)
-        elif(self.name == "Cannibals"):
-            freshCard = FightCard("weapon", 4, "none")
-        else:
-            freshCard = "error"
-        return freshCard
-    #Increases alert level by 1
-    def increaseAlert(self):
-        self.alertLevel += 1
-    def decreaseAlert(self):
-        self.alert -= 1
-class Cannibals(Hazard):
-    def __init__(self, name, reward, alertLevel, greenScore=5, yellowScore=9, redScore=14, drawCount=5):
-        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
-        self.name = "Cannibals"
-        self.reward = "Weapon"
-        self.alertLevel = alertLevel
-class WildAnimal(Hazard):
-    def __init__(self, name, reward, alertLevel, greenScore=4, yellowScore=7, redScore=11, drawCount=4):
-        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
-        self.name = "Wild Animal"
-        self.reward = reward
-        self.alertLevel = alertLevel
-class FurtherExplore(Hazard):
-    def __init__(self, name, reward, alertLevel, greenScore=2, yellowScore=5, redScore=8, drawCount=3):
-        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
-        self.name = "Further Explore"
-        self.reward = reward
-        self.alertLevel = alertLevel
-class Explore(Hazard):
-    def __init__(self, name, reward, alertLevel, greenScore=1, yellowScore=3, redScore=6, drawCount=2):
-        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
-        self.name = "Explore"
-        self.reward = reward
-        self.alertLevel = alertLevel
-class Raft(Hazard):
-    def __init__(self, name, reward, alertLevel, greenScore = 0, yellowScore = 1, redScore = 3, drawCount= 1):
-        super().__init__(name, reward, alertLevel, greenScore, yellowScore, redScore, drawCount)
-        self.name = "Raft"
-        self.reward = reward
-        self.alertLevel = alertLevel
-class Pirates(Hazard):
-    def __init__(self, name, diffScore, Reward,alertLevel):
-        super().__init__(name,diffScore,Reward, alertLevel)
 def displayFightCard(card):
     print("Name: ", card.name, "Score: ", card.fightScore, "Ability: ", card.ability)
 def displayHazard(card):
@@ -235,67 +168,6 @@ def startHazard(cards, discards, fightDeck, fightDeckDiscards, missionList):
 #NEEDS FUNCTION TO ADD AGING CARDS
 #NEED TO TEST CONVERT HAZARD TO REWARD
     #TO DO - FUNCTION RETURNS LIFE, LIFE FUNCTION TAKES AS INPUT TO DECREASE LIFE IF GREATER THAN 0
-#Creates fightCards with their constructors to be played in the game
-#Returns array filled with starting fightCards
-def createFightCardsDeck():
-    fightDeck = []
-    for i in range(0, 8):
-        fightDeck.append(Weak())
-    for i in range(0,5):
-        fightDeck.append(Distracted())
-    for i in range(0,3):
-        fightDeck.append(Focused())
-    fightDeck.append(Genius())
-    fightDeck.append(Eating())
-    return fightDeck
-#Creates Hazards with their constructors to be played in the game
-#Returns array filled with Hazards
-def createHazardsDeck():
-    # Creates 10 different Raft Cards
-    raft1 = Raft("Raft", "strategy", 0)
-    raft2 = Raft("Raft", "mimicry", 0)
-    raft3 = Raft("Raft", "realization", 0)
-    raft4 = Raft("Raft", "deception", 0)
-    raft5 = Raft("Raft", "equipment", 0)
-    # another unknown effect
-    raft6 = Raft("Raft", "???", 0)
-    raft7 = Raft("Raft", "book", 0)
-    raft8 = Raft("Raft", "food", 0)
-    raft9 = Raft("Raft", "food", 0)
-    raft10 = Raft("Raft", "food", 0)
-    raft11 = Raft("Raft", "equipment", 0)
-    # Creates 8 different Explore Cards
-    explore1 = Explore("Explore", "repeat", 0)
-    explore2 = Explore("Explore", "food", 0)
-    explore3 = Explore("Explore", "deception", 0)
-    explore4 = Explore("Explore", "weapon", 0)
-    explore5 = Explore("Explore", "mimicry", 0)
-    explore6 = Explore("Explore", "food", 0)
-    explore7 = Explore("Explore", "realization", 0)
-    explore8 = Explore("Explore", "weapon", 0)
-
-    # Creates 6 different Further Explore Cards
-    furtherExplore1 = FurtherExplore("Further Explore", "realization", 0)
-    furtherExplore2 = FurtherExplore("Further Explore", "experience", 0)
-    furtherExplore3 = FurtherExplore("Further Explore", "food", 0)
-    furtherExplore4 = FurtherExplore("Further Explore", "vision", 0)
-    furtherExplore5 = FurtherExplore("Further Explore", "repeat", 0)
-    furtherExplore6 = FurtherExplore("Further Explore", "strategy", 0)
-    # Creates 4 different Animal Cards
-    wild1 = WildAnimal("Wild Animal", "vision", 0)
-    wild2 = WildAnimal("Wild Animal", "experience", 0)
-    wild3 = WildAnimal("Wild Animal", "red", 0)
-    # Not sure what this ability is
-    wild4 = WildAnimal("Wild Animal", "strategy", 0)
-    # Creates 2 Cannibal Cards
-    cannibal1 = Cannibals("Cannibals", "Weapon", 0)
-    cannibal2 = Cannibals("Cannibals", "Weapon", 0)
-    hazardsDeck = [raft1, raft2, raft3, raft4, raft5, raft6, raft7, raft8, raft9, raft10,
-                   explore1, explore2, explore3, explore4, explore5, explore6, explore7, explore8,
-                   furtherExplore1, furtherExplore2, furtherExplore3, furtherExplore4, furtherExplore5, furtherExplore6,
-                   wild1, wild2, wild3, wild4, cannibal1, cannibal2
-                   ]
-    return hazardsDeck
 def createAgeDeck():
     ageDeck = []
     ageDeck.append(Suicidal())
@@ -327,96 +199,6 @@ def addCard(deck):
     pass
     #
 
-class FightCard:
-    def __init__(self, name, fightScore, ability):
-        self.name = name
-        self.fightScore = fightScore
-        self.tapped = 0
-        self.ability = ability
-    #Returns name and effect of ability
-    def getAbility(self):
-        pass
-
-    #Tapps the card
-    def tapCard(self):
-        self.tapped = 1
-    #Untaps Card
-    def unTapCard(self):
-        self.tapped = 0
-    def isTapped(self):
-        if(self.tapped == 0):
-            return False
-        return True
-    #checks names and details card ability
-    #UNTESTED
-    def printAbility(self):
-    #Checks cards ability and performs action
-        if(self.ability == "food"):
-            print("Food: Restores 1 life point")
-        if(self.ability == "eating"):
-            print("Food: Restores 2 life point")
-        elif(self.ability == "Realization"):
-            print("realization, destroy one card from mission")
-        elif(self.ability == "Vision"):
-            print("vision, Draw 3 cards, sort them, put at top of deck")
-        elif(self.ability == "mimicry"):
-            print("Copy one cards ability")
-        elif(self.ability == "equipment"):
-            print("Draw 2 cards")
-        elif(self.ability == "weapon"):
-            print("No Effect")
-        elif(self.ability == "repeat"):
-            print("double fight score")
-        elif(self.ability == "book"):
-            print("???")
-        elif(self.ability == "strategy"):
-            print("Exchange two cards from mission and deck")
-        elif(self.ability == "phase"):
-            print("Drop down phase for mission")
-        elif(self.ability == "experience"):
-            print("Plus one Card")
-            print("Two fight Score")
-        else:
-            print("There seems to be a mistake")
-        #if Ability == Copy
-            #Double score of highest card in mission[]
-
-#Uses Inheritance to add specific types of cards
-#8
-#1 eating
-class Weak(FightCard):
-    def __init__(self, name="weak", fightScore=0, ability="none"):
-        super().__init__(name,fightScore, ability)
-#5
-class Distracted(FightCard):
-    def __init__(self,name="distracted", fightScore="-1",ability="none"):
-        super().__init__(name, fightScore, ability)
-        self.ability = ability
-class Stupid(FightCard):
-    def __init__(self, name = "very stupid", fightScore = -3, ability = "none"):
-        super().__init__(name,fightScore, ability)
-        self.ability = ability
-#3
-class Focused(FightCard):
-    def __init__(self,name = "focused", fightScore = 1,ability = "none"):
-        super().__init__(name, fightScore, ability)
-#1
-class Genius(FightCard):
-    def __init__(self,name = "genius", fightScore = 2,ability = "none"):
-        super().__init__(name, fightScore, ability)
-class Eating(FightCard):
-    def __init__(self, name="eating", fightScore=0, ability="eating"):
-        super().__init__(name, fightScore, ability)
-    def useEat(self):
-        if not self.isTapped():
-            global lifePoints
-            lifePoints += 2
-            self.tapCard()
-        else:
-            print("Card already tapped")
-class Weak(FightCard):
-    def __init__(self, name="weak", fightScore=0, ability="none"):
-        super().__init__(name, fightScore, ability)
 def unTapMission(missionList):
     for i in missionList:
         i.unTapCard()
@@ -437,69 +219,6 @@ def useAbility(missionList, abilityCard, fightDeck, hazardsDeck):
             changeMimic("mimicry")
         """""
 
-#Destroys card during a mission
-def useRealization(missionList, fightCard):
-    if(fightCard.isTapped() == True):
-        print("Card already used")
-    displayMissionList(missionList)
-    choice = int(input("Choose card to delete"))
-    fightCard.tapCard()
-    missionList.pop(choice-1)
-class Realization(FightCard):
-    def __init__(self, name="Realization", fightScore=0, ability="Realization"):
-        super().__init__(name, fightScore, ability)
-#Allows a vision card to reorganize top 3 cards of deck
-def useVision(fightDeck, fightCard):
-    if(fightCard.isTapped() == True):
-        print("Card already used")
-    temp = []
-    for i in range(0,3):
-        temp.append(drawFightCard(fightDeck))
-        displayFightCard(temp[i])
-    choice1 = int(input("Enter the first card you want to see in deck"))
-    temp.insert(0, temp.pop(choice1-1))
-    for i in range(0,3):
-        displayFightCard(temp[i])
-    choice2 = int(input("Enter the second card you want to see in deck"))
-    temp.insert(1, temp.pop(choice2-1))
-    for i in range(0,3):
-        displayFightCard(temp[i])
-    choice3 = int(input("Enter the third card you want to see in deck"))
-    temp.insert(2, temp.pop(choice3-1))
-    for j in range(0, len(fightDeck)):
-        temp.append(drawFightCard(fightDeck))
-    for i in range(0,3):
-        displayFightCard(temp[i])
-    fightCard.tapCard()
-    return temp
-
-
-class Vision(FightCard):
-    def __init__(self, name="Vision", fightScore=0, ability="Vision"):
-        super().__init__(name, fightScore, ability)
-#Allows card to mimic, AKA, take ability until mission ends
-def useMimic(missinList, fightCard):
-    if(fightCard.isTapped()):
-        print("Must be a mimic card")
-    else:
-        displayMissionList(missionList)
-        choice = int(input("Which card do you want to mimic"))
-        #Use Ability of ML[choice-1]
-    fightCard.tapCard()
-class Mimicry(FightCard):
-    def __init__(self, name="Mimicry", fightScore=0, ability="Copy"):
-        super().__init__(name, fightScore, ability)
-#10 cards
-#I haven't recorded all 10 of these cards yet
-class Suicidal(FightCard):
-    def __init__(self, name = "Suicidal", fightScore = -2, ability = "none"):
-        super().__init__(name,fightScore, ability)
-        self.age = True
-class Stupid(FightCard):
-    def __init__(self, name ="Stupid", fightScore = -2, ability = "none"):
-        super().__init__(name,fightScore, ability)
-        self.age = True
-
 
 #print("Friday is a game about understanding chance and probability of cards to optimize your chances of survival")
 #print("Robinson Crusoe has been stranded on an island for weeks, help guide him against the trecherous hazards")
@@ -507,6 +226,8 @@ ageDeck = createAgeDeck()
 hazardsDeck = createHazardsDeck()
 fightDeck = createFightCardsDeck()
 shuffle(fightDeck)
+"""
+
 smallDeck1 = []
 smallDeck2 = []
 smallDeck2.append(Vision())
@@ -516,6 +237,7 @@ print(smallDeck2[0].printAbility())
 smallDeck1.append(Raft("raft", "Strategy", 0))
 testDiscard = list(hazardsDeck)
 testDiscard2 = list(fightDeck)
+"""
 while(True):
     break
     print("---------------DISCARD PILE----------------")
